a725aec94d8959df36d2e8187d45ecc5ba868b2b
Jenna Van De Vyver <r0889235@student.thomasmore.be>
2023-10-18 22:02:19 +0200

    added assets for courseImages + ScrollView container + tileContainer with 1 tile to test

--- App.js
+++ App.js
@@ -1,21 +1,67 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, ScrollView } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Text>Fklsdnfmbùqsbf!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ScrollView style={styles.container}>
+      <Text style={styles.header1}>Courses</Text>
+      <View style={styles.tilesContainer}>
+        <View styles={styles.tile}>
+          <Image style={styles.tileImage} source={require('./assets/courseImages/macbook-code.jpg')}/>
+          <View style={styles.tileDetail}>
+            <Text style={styles.tileHeader}>Development 1</Text>
+            <Text style={styles.tileText}>(YP0584 | 2022)</Text>
+          </View>
+        </View> 
+      </View>
+
+    </ScrollView>
+
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  header1: {
+    fontSize: 32,
+    fontWeight: '800',
+    margin: 12,
   },
+  container: {
+    backgroundColor: '#fff',
+    paddingTop: 56,
+  },
+  tilesContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    flexGrow: 0,
+    flexShrink: 0,
+    justifyContent: 'space-evenly',
+
+  },
+  tile: {
+    flexBasis: '46%',//200
+    borderRadius: 5,
+    borderColor: '#B4B8DA',
+    borderWidth: 1,
+    borderStyle: 'solid',
+    marginVertical: '2%',
+
+  },
+  tileImage: {
+    width: '100%',
+    maxHeight: 110,
+    overflow: 'hidden',
+  },
+  tileDetail: {
+    padding: 10,
+  },
+  tileHeader: {
+    fontSize: 22,
+    fontWeight: '600',
+  },
+  tileText: {
+    fontSize: 16,
+    fontWeight: '500',
+  }
 });
