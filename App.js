f1c797f23359c24809e022b1c578e55db9f50db2
Jenna Van De Vyver <r0889235@student.thomasmore.be>
2023-10-18 22:52:27 +0200

    App.js - added all the tiles + note - text sets size of tile

--- App.js
+++ App.js
@@ -15,7 +15,7 @@ export default function App() {
           <Image style={styles.tileImage} source={require('./assets/courseImages/macbook-code.jpg')}/>
           <View style={styles.tileDetail}>
             <Text style={styles.tileHeader}>Development 1</Text>
-            <Text style={styles.tileText}>(YP0584 | 2022)</Text>
+            <Text style={styles.tileText}>(YP0584 | 2023)</Text>
           </View>
         </View>
 
@@ -24,9 +24,83 @@ export default function App() {
           <Image style={styles.tileImage} source={require('./assets/courseImages/sensors.jpg')}/>
           <View style={styles.tileDetail}>
             <Text style={styles.tileHeader}>Development 2</Text>
-            <Text style={styles.tileText}>(YP0859 | 2022)</Text>
+            <Text style={styles.tileText}>(YP0859 | 2023)</Text>
           </View>
         </View>
+
+        {/* tile nr.3 */}
+        <View styles={styles.tile}>
+          <Image style={styles.tileImage} source={require('./assets/courseImages/phone-desk.jpg')}/>
+          <View style={styles.tileDetail}>
+            <Text style={styles.tileHeader}>Development 3</Text>
+            <Text style={styles.tileText}>(YP0833 | 2023)</Text>
+          </View>
+        </View>
+
+        {/* tile nr.4 */}
+        <View styles={styles.tile}>
+          <Image style={styles.tileImage} source={require('./assets/courseImages/php.jpg')}/>
+          <View style={styles.tileDetail}>
+            <Text style={styles.tileHeader}>Development 4</Text>
+            <Text style={styles.tileText}>(YP0860 | 2023)</Text>
+          </View>
+        </View>
+
+        {/* tile nr.5 */}
+        <View styles={styles.tile}>
+          <Image style={styles.tileImage} source={require('./assets/courseImages/idea.jpg')}/>
+          <View style={styles.tileDetail}>
+            <Text style={styles.tileHeader}>Lab             1</Text>
+            <Text style={styles.tileText}>(YP0584 | 2023)</Text>
+          </View>
+        </View>
+
+        {/* tile nr.6 */}
+        <View styles={styles.tile}>
+          <Image style={styles.tileImage} source={require('./assets/courseImages/ideas.jpg')}/>
+          <View style={styles.tileDetail}>
+            <Text style={styles.tileHeader}>Lab             2</Text>
+            <Text style={styles.tileText}>(YP0587 | 2023)</Text>
+          </View>
+        </View>
+
+        {/* tile nr.7 */}
+        <View styles={styles.tile}>
+          <Image style={styles.tileImage} source={require('./assets/courseImages/macbook-code.jpg')}/>
+          <View style={styles.tileDetail}>
+            <Text style={styles.tileHeader}>Lab             3</Text>
+            <Text style={styles.tileText}>(YP0595 | 2023)</Text>
+          </View>
+        </View>
+
+        {/* tile nr.8 */}
+        <View styles={styles.tile}>
+          <Image style={styles.tileImage} source={require('./assets/courseImages/sketch.jpg')}/>
+          <View style={styles.tileDetail}>
+            <Text style={styles.tileHeader}>Creatie      1</Text>
+            <Text style={styles.tileText}>(YP0563 | 2023)</Text>
+          </View>
+        </View>
+
+        {/* tile nr.9 */}
+        <View styles={styles.tile}>
+          <Image style={styles.tileImage} source={require('./assets/courseImages/graduate.jpg')}/>
+          <View style={styles.tileDetail}>
+            <Text style={styles.tileHeader}>Creatie      2</Text>
+            <Text style={styles.tileText}>(YP0863 | 2023)</Text>
+          </View>
+        </View>
+
+        {/* tile nr.10 */}
+        <View styles={styles.tile}>
+          <Image style={styles.tileImage} source={require('./assets/courseImages/library.jpeg')}/>
+          <View style={styles.tileDetail}>
+            <Text style={styles.tileHeader}>Creatie      3</Text>
+            <Text style={styles.tileText}>(YP0833 | 2023)</Text>
+          </View>
+        </View>
+
+        <StatusBar style="auto" />
 
         {/* end tilesContainer */} 
       </View>
